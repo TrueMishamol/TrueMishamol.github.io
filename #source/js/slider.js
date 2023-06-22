ca513943ec23821138caf5f9aceb40ec9a389200
Mishamol <101403719+TrueMishamol@users.noreply.github.com>
2023-06-22 23:56:43 +0300

    feat: Slider fits selected element size
    + refactor

--- #source/js/slider.js
+++ #source/js/slider.js
@@ -1,103 +1,132 @@
-let { sliders, imageCount, images } = slider();
+let { sliders, selectedElementNumber, elementsCount } = initializeSlider();
 
-function slider() {
+function initializeSlider() {
 
-    let sliders = document.querySelectorAll('.slider'); //Ищет все слайдеры в массив
-    let imageCount = [];
-    let images = [];
- 
+    let sliders = document.querySelectorAll('.slider'); // Массив всех слайдеров на странице
+    let selectedElementNumber = []; // [Номер cлайдера] = Номер выбранного элеменна слайдера
+    let elementsCount = []; // [Номер cлайдера] = Количество элементов слайдера
+
     // for each slider on a page
     for (let i = 0; i < sliders.length; i++) {
 
-        // Инициализирует каждый слайдер
-        imageCount[i] = 0;
-        let imagesTmp = sliders[i].querySelectorAll('.slider__container>*');
-        images[i] = imagesTmp.length;
+        let sliderElements = sliders[i].querySelectorAll('.slider__container>*'); // Массив элементов слайдера
+
+        // Заполнение массивов
+        selectedElementNumber[i] = 0;
+        elementsCount[i] = sliderElements.length; // Количество элементов слайдера
 
         // Слушаем кнопки
         sliders[i].querySelector('.slider__button_next').addEventListener('click', function () {
-            imageCount[i]++;
+            selectedElementNumber[i]++;
             rollSlider(i);
         });
         sliders[i].querySelector('.slider__button_prev').addEventListener('click', function () {
-            imageCount[i]--;
+            selectedElementNumber[i]--;
             rollSlider(i);
         });
 
         // Swipe
-        sliders[i].addEventListener('touchstart', handleTouchStart, false);
-        sliders[i].addEventListener('touchmove', handleTouchMove, false);
-        sliders[i].addEventListener('touchend', handleTouchEnd, false);
-        let x1 = null;
-        let x2 = null;
+        handleSwipe();
+        function handleSwipe() {
+            sliders[i].addEventListener('touchstart', handleTouchStart, false);
+            sliders[i].addEventListener('touchmove', handleTouchMove, false);
+            sliders[i].addEventListener('touchend', handleTouchEnd, false);
+            let x1 = null;
+            let x2 = null;
 
-        function handleTouchStart(event) {
-            x1 = event.touches[0].clientX;
-        }
-
-        function handleTouchMove(event) {
-            x2 = event.touches[0].clientX;
-        }
-
-        function handleTouchEnd() {
-            if (x1 - x2 > 50 & x2 != null) {
-                imageCount[i]++;
+            function handleTouchStart(event) {
+                x1 = event.touches[0].clientX;
             }
-            if (x1 - x2 < -50) {
-                imageCount[i]--;
+
+            function handleTouchMove(event) {
+                x2 = event.touches[0].clientX;
             }
-            rollSlider(i);
 
-            x1 = null;
-            x2 = null;
-        }
-
-        window. onload = function() {
-            for (let i = 0; i < sliders.length; i++) {
+            function handleTouchEnd() {
+                if (x1 - x2 > 50 & x2 != null) {
+                    selectedElementNumber[i]++;
+                }
+                if (x1 - x2 < -50) {
+                    selectedElementNumber[i]--;
+                }
                 rollSlider(i);
+
+                x1 = null;
+                x2 = null;
             }
         }
     }
-    return { sliders, imageCount, images };
+
+    // После загрузки - для каждого слайдера
+    window.onload = function () {
+        for (let i = 0; i < sliders.length; i++) {
+            rollSlider(i);
+        }
+    }
+
+    return { sliders, selectedElementNumber, elementsCount };
 }
 
-function rollSlider(i){
+function rollSlider(i) {
     let sliderContainer = sliders[i].querySelector('.slider__container');
     let width; // Для одной картинки
     width = sliders[i].querySelector('.slider__window').offsetWidth;
 
-    if (imageCount[i] >= images[i]) {
-        imageCount[i] = 0;
+    if (selectedElementNumber[i] >= elementsCount[i]) {
+        selectedElementNumber[i] = 0;
     }
 
-    if (imageCount[i] < 0){
-        imageCount[i] = images[i] - 1;
+    if (selectedElementNumber[i] < 0) {
+        selectedElementNumber[i] = elementsCount[i] - 1;
     }
 
-    sliderContainer.style.transform = 'translate(-' + imageCount[i] * width + 'px)';
+    sliderContainer.style.transform = 'translate(-' + selectedElementNumber[i] * width + 'px)';
 
     // Arrow buttons position height
-    let height;
-    height = sliderContainer.offsetHeight;
-    let sliderButton = sliders[i].querySelector('.slider__button');
-    let sliderButtonPrev = sliders[i].querySelector('.slider__button .slider__button_prev');
-    let sliderButtonNext = sliders[i].querySelector('.slider__button .slider__button_next');
+    setSliderButtonsPosition(i);
 
-    if (sliderButton != null) {
-        sliderButtonPrev.style.height = ' ' + width/10 + 'px'; //10vw
-        sliderButtonPrev.style.bottom = ' ' + (height/2 + width/20) + 'px'; //40vw
-
-        sliderButtonNext.style.height = ' ' + width/10 + 'px'; //10vw 
-        sliderButtonNext.style.bottom = ' ' + (height/2 + width/20) + 'px'; //40vw
-    }
+    fitSelectedElementHeight(i);
 };
 
 resize();
 
+// When screen resize
 function resize() {
     window.addEventListener('resize', function () {
         for (let i = 0; i < sliders.length; i++) {
             rollSlider(i);
         }
     });
+}
+
+function setSliderButtonsPosition(i) {
+    let width; // Для одной картинки
+    let height;
+    let sliderElements = sliders[i].querySelectorAll('.slider__container>*'); // Массив элементов слайдера
+    let elementNumber = selectedElementNumber[i];
+    let elementHeight = sliderElements[elementNumber].offsetHeight;
+
+    let sliderButton = sliders[i].querySelector('.slider__button');
+    let sliderButtonPrev = sliders[i].querySelector('.slider__button .slider__button_prev');
+    let sliderButtonNext = sliders[i].querySelector('.slider__button .slider__button_next');
+
+    width = sliders[i].querySelector('.slider__window').offsetWidth;
+    height = elementHeight;
+
+    if (sliderButton != null) {
+        sliderButtonPrev.style.height = ' ' + width / 10 + 'px'; //10vw
+        sliderButtonPrev.style.bottom = ' ' + (height / 2 + width / 20) + 'px'; //40vw
+
+        sliderButtonNext.style.height = ' ' + width / 10 + 'px'; //10vw 
+        sliderButtonNext.style.bottom = ' ' + (height / 2 + width / 20) + 'px'; //40vw
+    }
+}
+
+function fitSelectedElementHeight(sliderNumber) {
+    let sliderElements = sliders[sliderNumber].querySelectorAll('.slider__container>*'); // Массив элементов слайдера
+    let sliderContainer = sliders[sliderNumber].querySelector('.slider__container');
+    let elementNumber = selectedElementNumber[sliderNumber];
+    let elementHeight = sliderElements[elementNumber].offsetHeight;
+
+    sliderContainer.style.height = elementHeight + 'px';
 }
