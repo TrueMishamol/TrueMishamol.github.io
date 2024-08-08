16c563745e5a23da6dbdcb90f116a13156f74fab
Mikhail Molodozhenov (Mishamol) <reg+github@mishamol.ru>
2024-08-08 16:54:40 +0300

    Clean Up + Button Small Fixes

--- mishamol.ru/#source/js/popup.js
+++ mishamol.ru/#source/js/popup.js
@@ -15,24 +15,16 @@ buttons.forEach(button => {
 // Close button
 document.querySelectorAll('.popup_close').forEach(button => {
 	button.addEventListener('click', function () {
-		const popup = this.closest('.popup'); // Находим поп-ап
+		const popup = this.closest('.popup'); // Finding a popup
 
-		// Убираем класс show для начала анимации исчезновения
+		// Begin Hiiding
 		popup.classList.add('hiding');
 		popup.classList.remove('show');
 
-		// // Устанавливаем таймер на 0.5 секунды
-		// setTimeout(() => {
-		// 	popup.classList.remove('hiding');
-		// }, 500); // 500 миллисекунд
-
-
-		// Добавляем обработчик события transitionend
+		// On Transitionend
 		popup.addEventListener('transitionend', function () {
-			// После завершения анимации скрываем поп-ап
-			// popup.style.visibility = 'hidden'; // Скрываем поп-ап
-			popup.classList.remove('hiding');
+			popup.classList.remove('hiding'); // End Hiding. Completely Hide element 
 
-		}, { once: true }); // Удаляем обработчик после первого срабатывания
+		}, { once: true }); // Remove the handler after the first trigger
 	});
 });
